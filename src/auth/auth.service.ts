6df3cf892baba823dd13c954091daa6224aeb22d
Richard Rodriguez <rrodriguez@comercializadora-s3.com>
2024-03-21 07:51:31 -0500

    - add create user

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class AuthService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
   create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
+
+    const newUser = this.usersRepository.create({
+      ...createAuthDto
+    });
+
+    return newUser
+
   }
 
   findAll() {
--- src/auth/dto/create-auth.dto.ts
+++ src/auth/dto/create-auth.dto.ts
@@ -1,1 +1,15 @@
-export class CreateAuthDto {}
+import { IsEmail, IsNotEmpty, MinLength } from "class-validator";
+
+export class CreateAuthDto {
+
+    @IsEmail()
+    email: string;
+
+    @IsNotEmpty()
+    @MinLength(5)
+    password: string;
+
+    @IsNotEmpty()
+    fullName: string;
+
+}
